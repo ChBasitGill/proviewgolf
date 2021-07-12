b00536085e8c6286805c8a3c77dd7b1f1ace0c6a
Basit <chbasitgill@outlook.com>
2021-07-13 02:26:20 +0500

    Innvitation

---
+++ proviewgolf_web/ProViewGolf.Core/Dbo/Models/InvitationModel.cs
@@ -0,0 +1,17 @@
+﻿using ProViewGolf.Core.Dbo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProViewGolf.Core.Dbo.Models
+{
+    public class InvitationModel
+    {
+        public string StudentEmail { get; set; }
+        public string InstructorEmail { get; set; }
+
+        public string Code { get; set; } = Guid.NewGuid().ToString();
+        public InvitationStatus Status { get; set; } = InvitationStatus.Pending;
+        public Pro Pro { get; set; }
+    }
+}
